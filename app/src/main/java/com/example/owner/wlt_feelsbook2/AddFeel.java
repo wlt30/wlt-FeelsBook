001aff75ef7f574dd6ef27bb4fab6670d7309525
wlt30 <wlt@ualberta.ca>
2018-10-04 13:24:04 -0600

    following videos, made main and add_mood screens

--- app/src/main/java/com/example/owner/wlt_feelsbook2/AddFeel.java
+++ app/src/main/java/com/example/owner/wlt_feelsbook2/AddFeel.java
@@ -10,7 +10,7 @@ public class AddFeel extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_feel);
+        setContentView(R.layout.main);
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
 
--- app/src/main/java/com/example/owner/wlt_feelsbook2/FeelsList.java
+++ app/src/main/java/com/example/owner/wlt_feelsbook2/FeelsList.java
@@ -1,4 +1,36 @@
 package com.example.owner.wlt_feelsbook2;
 
-public class FeelsList {
+import java.util.ArrayList;
+
+public class FeelsList implements MyObservable, MyObserver{
+    private Feel mostRecentFeel;
+    private ArrayList<Feel> feels = new ArrayList<>();
+
+    public void add(Feel feel){
+        mostRecentFeel = feel;
+        feels.add(feel);
+        feel.addObserver(this);
+        notifyAllObservers();
+    }
+
+    public Feel getMostRecentFeel(){
+        return mostRecentFeel;
+    }
+
+    public int count(){return feels.size();}
+
+    private volatile ArrayList<MyObserver> observers = new ArrayList<>();
+
+    public void addObserver(MyObserver observer){
+        observers.add(observer);
+    }
+    private void notifyAllObservers() {
+        for (MyObserver observer : observers) {
+            observer.myNotify(this);
+        }
+    }
+
+    public void myNotify(MyObservable observable){
+        notifyAllObservers();
+    }
 }
