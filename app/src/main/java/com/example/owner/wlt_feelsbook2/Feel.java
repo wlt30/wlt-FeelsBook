869486936d15097408d955974d52d2505e56a785
wlt30 <wlt@ualberta.ca>
2018-10-05 04:21:38 -0600

    edited EditFeelActivity

--- app/src/main/java/com/example/owner/wlt_feelsbook2/Feel.java
+++ app/src/main/java/com/example/owner/wlt_feelsbook2/Feel.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 
 public class Feel extends Object implements MyObservable {
-    protected String comment = "(optional)";
+    protected String comment;
     protected String mood;
     protected Date date; //this was protected before, but we want it to be editable
 
@@ -25,21 +25,7 @@ public class Feel extends Object implements MyObservable {
 
     public String getComment() {return comment;}
     public String getMood() {return mood;}
-    public String getDate() {
-        Date testDate = this.date;
-        DateFormat dformat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
-        String newDate = dformat.format(new Date());
-        return newDate;
-    }
-
-/*    public void setComment(String comment) throws FeelTooLongException{
-        if (comment.length() <= 100) {
-            this.comment = comment;
-        } else{
-            throw new FeelTooLongException();
-        }
-        notifyAllObservers();
-    }*/
+    public String getDate() { return dateToString(); }
 
     public void setMood(String mood) throws FeelTooLongException{
             this.mood = mood;
@@ -51,8 +37,7 @@ public class Feel extends Object implements MyObservable {
         notifyAllObservers();
     }
 
-    @Override
-    public String toString() {
+    public String dateToString() {
         Date testDate = this.date;
         DateFormat dformat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
         String newDate = dformat.format(new Date());
